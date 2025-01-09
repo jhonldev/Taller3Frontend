31c66b743709feb3995a94b6cf488710a77414d0
jhonldev <jhon.lopez.developer@gmail.com>
2025-01-09 19:55:15 -0300

    Configurar despliegue automático en Firebase Hosting

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { store } from "../store/store";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://taller3-sc1g.onrender.com";
 
 const responseBody = (response) => response.data;
 axios.defaults.withCredentials = true;
@@ -58,6 +58,7 @@ const GetReservations = {
 const Search = {
   searchUser: (searchData) => requests.post("user/searchUser", { searchData }),
 };
+
 
 const agent = {
   Login,
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_tCMATlcqEKiIvfmeLu1HqRw6wIfbuP4",
+  authDomain: "frontend-bcd0f.firebaseapp.com",
+  projectId: "frontend-bcd0f",
+  storageBucket: "frontend-bcd0f.firebasestorage.app",
+  messagingSenderId: "239932765610",
+  appId: "1:239932765610:web:e2322de187af49a162a7d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
